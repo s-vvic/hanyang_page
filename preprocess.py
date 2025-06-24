7b4b236c09e4202a1b57b957e8d27d568a3bea39
손우승 <thsdntmd6547@gmail.com>
2025-06-24 14:50:44 +0900

    TF-IDF 테스트 코드

--- preprocess.py
+++ preprocess.py
@@ -5,8 +5,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def Cal_tfidf(docs):
     f = open(f'documents/{docs}', 'r', encoding='UTF-8')
-    lines = f.read()
+    lines = f.read().splitlines()
     f.close()
-    return lines
+    print(lines)
+
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(lines)
+
+    similarities = cosine_similarity(tfidf_matrix[0], tfidf_matrix)
+    return similarities
 
 print(Cal_tfidf("test.txt"))
